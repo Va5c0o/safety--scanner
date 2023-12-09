9455d872f71b277a2d7785fc23ec4a8cfe26de96
Vasco Overes <vascooveres@gmail.com>
2023-12-09 14:43:25 +0100

    I added a form for every page that needs it. And i started adding a db

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,34 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-  return "<p>Login</p>"
+  # data = request.form
+  # print(data)
+  return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
-  return "<p>Signup</p>"
+  if request.method == 'POST':
+     email = request.form.get('email')
+     firstName = request.form.get('firstName')
+     password1 = request.form.get('password1')
+     password2 = request.form.get('password2')
+
+     if len(email) < 4:
+        flash('Email must be greater than 4 characters', category='error')
+     elif len(firstName) < 2:
+        flash('First name must be greater than 2 characters', category='error')
+     elif password1 != password2:
+        flash('Password don\'t match!', category='error')
+     elif len(password1) < 7:
+        flash('Password must be greater than 7 characters!', category='error')
+     else:
+        flash('Account created!', category='success')
+        pass
+  return render_template("signup.html")
