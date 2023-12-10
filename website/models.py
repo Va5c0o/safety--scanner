376272f0c890c741a053615703ccee476b671a55
Vasco Overes <vascooveres@gmail.com>
2023-12-10 16:38:18 +0100

    Last final parts and making it possible to add the notes and delete the notes

--- website/models.py
+++ website/models.py
@@ -1,4 +1,4 @@
-from . import db
+from website import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
@@ -6,10 +6,10 @@ class Note(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   data = db.Column(db.String(10000))
   date = db.Column(db.DateTime(timezone=True), default=func.now())
-  user_id = db.Column(db.Interger, db.ForeignKey('user.id'))
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class User(db.Model, UserMixin):
-  id = db.Column(db.Interger, primary_key=True)
+  id = db.Column(db.Integer, primary_key=True)
   email = db.Column(db.String(150), unique=True)
   password = db.Column(db.String(150))
   first_name = db.Column(db.String(150))
